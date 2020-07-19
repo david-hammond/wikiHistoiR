678449b0ec147f08d0fbb178a2edb067e545c375
david-hammond <anotherdavidhammond@gmail.com>
2020-07-19 13:50:12 +1000

    error checking

--- tester.R
+++ tester.R
@@ -15,11 +15,10 @@ get_people = function(wikipage){
   print(wikipage)
   url = paste0("https://en.wikipedia.org/wiki/", wikipage)
   webpage <- try(read_html(url))
-  WikipediaR::links(wikipage)
   if(class(webpage) != "try-error"){
     table <- webpage %>%
       html_nodes(xpath='//*[@class="infobox biography vcard"]') %>%
-      html_table(header=F)
+      html_table(header = F, fill = TRUE)
     if(length(table)>0){
       vcard <- table[[1]]
     }
