040f3e6d90e6b10199b0cbd87ba217b4b39e3771
david-hammond <anotherdavidhammond@gmail.com>
2020-07-22 15:22:11 +1000

    minor lapply fix

--- tester.R
+++ tester.R
@@ -6,7 +6,7 @@ library(padr)
 source("libs.R")
 get_bios = function(url){
   links = get_links(url)
-  people = pblapply(links, get_people)
+  people = lapply(links, get_people)
   formatted_people = people[!sapply(people, is.null, simplify = T)]
   formatted_people = bind_rows(formatted_people, .id = "person") %>%
     filter(descr %in% c("Born", "Died")) %>% select(1:3)
@@ -21,7 +21,6 @@ get_bios = function(url){
 url = c("https://en.wikipedia.org/wiki/Islam", "https://en.wikipedia.org/wiki/Judaism",  "https://en.wikipedia.org/wiki/Christianity")
 people = pblapply(url, get_bios)
 saveRDS(people, "people.rds")
-formatted_people = formatted_people %>% rbind(formatted_people %>% mutate(subject = "David"))
 timeline = formatted_people %>% group_by(subject, newyear) %>% summarise(num = n()) %>%
   ungroup()
 ggplot(timeline, aes(x = newyear, y = num, fill = subject)) + geom_area(stat = "smooth")
